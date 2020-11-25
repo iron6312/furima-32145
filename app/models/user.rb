74980a7d6e3d9aaf3029f5da95d409c8a167b237
iron6312 <edhardy312312@gmail.com>
2020-11-25 18:23:06 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,21 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :password, :password_confirmation, confirmation: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :last_name, presence: true
+  VALID_PASSWORD_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :first_name_kana, format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana, format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :first_name, format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name, format: { with: VALID_PASSWORD_REGEX }
 end
