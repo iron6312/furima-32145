fb9349d978353008e0ea8f36a60a0f33da55659b
iron6312 <edhardy312312@gmail.com>
2020-11-26 11:08:51 +0900

    with_optionsを使ったバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :birthday
+  end
+
   validates :email, uniqueness: { case_sensitive: false }
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :password, :password_confirmation, confirmation: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :last_name, presence: true
-  VALID_PASSWORD_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :first_name_kana, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :last_name_kana, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
-  validates :first_name, format: { with: VALID_PASSWORD_REGEX }
-  VALID_PASSWORD_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
-  validates :last_name, format: { with: VALID_PASSWORD_REGEX }
+
+  with_options format: {with: /\A[ァ-ヶー－]+\z/.freeze} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]/.freeze} do
+    validates :first_name
+    validates :last_name
+  end
 end
