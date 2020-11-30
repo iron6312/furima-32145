413aa40d1d63952517cb9dd70a12c34a2e7f9404
iron6312 <edhardy312312@gmail.com>
2020-11-30 12:42:25 +0900

    一覧表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    #@items = Item.includes(:user)
-    #@items = Item.order('created_at DESC')
+    @items = Item.includes(:user)
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    #@item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   private
