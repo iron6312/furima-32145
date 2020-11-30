7e834c5a838b99b9334e1e42d11ae2b95dfcd34e
iron6312 <edhardy312312@gmail.com>
2020-11-30 14:47:10 +0900

    出品なければダミー表示修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.includes(:user)
-    @items = Item.order('created_at DESC')
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -12,6 +11,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    # binding.rye
     if @item.save
       redirect_to root_path
     else
