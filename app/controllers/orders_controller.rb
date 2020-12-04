2a9c0d85ce3ceea73dfc0cc6786f3207a7e4f82b
iron6312 <edhardy312312@gmail.com>
2020-12-04 11:47:31 +0900

    手直し①

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
     @user_order = UserOrder.new
     redirect_to root_path if current_user.id == @item.user.id
 
-    redirect_to root_path if current_user != @item.history
+    redirect_to root_path if  @item.history != nil
   end
 
   def create
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = 'sk_test_50ea751ecc59ca38de135935'
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_72cbe59307b24bd7ce93f4f5");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
